5497a5e92232dbef8b72ebff65c1e5c17e334ea9
ulyssecrn <51723351+ulyssecrn@users.noreply.github.com>
2020-04-29 21:28:11 +0200

    Optimized version

--- linkedin-extractor.py
+++ linkedin-extractor.py
@@ -1,44 +1,9 @@
 import csv
 import time
 from selenium import webdriver
+from credentials import *
 
-user = ''
-pwd = ''
-group = ''
-
-driver = webdriver.Chrome()
-driver.get('https://linkedin.com')
-
-sign_in_button = driver.find_element_by_class_name('nav__button-secondary')
-sign_in_button.click()
-
-
-############# connection & ouverture de la page #############
-print('Logging in...')
-id_box = driver.find_element_by_id('username')
-id_box.send_keys('user')
-pass_box = driver.find_element_by_id('password')
-pass_box.send_keys('pwd')
-sign_in_button_2 = driver.find_element_by_class_name('btn__primary--large')
-sign_in_button_2.click()
-time.sleep(2)
-
-if "number" in driver.current_url:
-    skip_button = driver.find_element_by_class_name('secondary-action')
-    skip_button.click()
-    time.sleep(2)
-
-page = 1
-
-print('Finding group...')
-driver.get('https://www.linkedin.com/groups/'+group'/manage/membership/members/?page='+str(page))
-time.sleep(5)
-dismiss_button = driver.find_element_by_class_name("artdeco-dismiss")
-dismiss_button.click()
-driver.get('https://www.linkedin.com/groups/'+group'/manage/membership/members/?page='+str(page))
-time.sleep(5)
-
-############# définition des fonctions #############
+# functions
 def parsename(x):
     name = driver.find_element_by_xpath(
         "/html[@class='artdeco osx']/body[@class='render-mode-BIGPIPE nav-v2 ember-application boot-complete icons-loaded']/div[@id='ember6']/div[@class='application-outlet ']/div[@class='authentication-outlet']/div[@id='groups']/div[@class='groups-manage']/div[@class='groups-manage__grid']/main[@class='groups-manage__core-rail container ph0 pb0 Elevation-2dp']/div[@id='ember62']/artdeco-typeahead[@id='ember63']/artdeco-typeahead-results-list[@id='ember63-a11y']/artdeco-typeahead-result[@id='ember" + str(
@@ -52,21 +17,39 @@ def parsebio(x):
         x + 2) + "']/artdeco-entity-lockup-content[@id='ember" + str(x + 8) + "']/artdeco-entity-lockup-subtitle[@id='ember" + str(x + 12) + "']").text
     return bio
 
+# selenium webdriver, change it here if you use another browser
+driver = webdriver.Safari()
 
-############## par lettre de l'alphabet ################
-search = driver.find_element_by_xpath("/html[@class='artdeco osx']/body[@class='render-mode-BIGPIPE nav-v2 ember-application boot-complete icons-loaded']/div[@id='ember6']/div[@class='application-outlet ']/div[@class='authentication-outlet']/div[@id='groups']/div[@class='groups-manage']/div[@class='groups-manage__grid']/main[@class='groups-manage__core-rail container ph0 pb0 Elevation-2dp']/div[@id='ember62']/artdeco-typeahead[@id='ember63']/section[@class='groups-manage-group-list__tab-header ph4 display-flex align-items-flex-end']/div[@class='groups-manage-group-list__search relative']/div[@id='groups-manage-group-list__search-input']/div/input")
-search.send_keys('a')
-last_page = int(driver.find_element_by_xpath("/html[@class='artdeco osx']/body[@class='render-mode-BIGPIPE nav-v2 ember-application boot-complete icons-loaded']/div[@id='ember6']/div[@class='application-outlet ']/div[@class='authentication-outlet']/div[@id='groups']/div[@class='groups-manage']/div[@class='groups-manage__grid']/main[@class='groups-manage__core-rail container ph0 pb0 Elevation-2dp']/div[@id='ember62']/artdeco-typeahead[@id='ember63']/section[2]/artdeco-pagination[@class='artdeco-pagination pv5']/ul[@class='artdeco-pagination__pages artdeco-pagination__pages--number']/li[@class='artdeco-pagination__indicator artdeco-pagination__indicator--number '][8]/button/span").text)
-next_page = driver.find_element_by_xpath("/html[@class='artdeco osx']/body[@class='render-mode-BIGPIPE nav-v2 ember-application boot-complete icons-loaded']/div[@id='ember6']/div[@class='application-outlet ']/div[@class='authentication-outlet']/div[@id='groups']/div[@class='groups-manage']/div[@class='groups-manage__grid']/main[@class='groups-manage__core-rail container ph0 pb0 Elevation-2dp']/div[@id='ember62']/artdeco-typeahead[@id='ember63']/section[2]/artdeco-pagination[@class='artdeco-pagination pv5']/button[@id='ember1441']")
-next_page.click()
+# connection
+print('Logging in...')
+driver.get('https://linkedin.com')
+sign_in_button = driver.find_element_by_class_name('nav__button-secondary')
+sign_in_button.click()
+id_box = driver.find_element_by_id('username')
+id_box.send_keys('user')
+pass_box = driver.find_element_by_id('password')
+pass_box.send_keys('pwd')
+sign_in_button_2 = driver.find_element_by_class_name('btn__primary--large')
+sign_in_button_2.click()
+time.sleep(2)
 
+# accept warning
+if "number" in driver.current_url:
+    skip_button = driver.find_element_by_class_name('secondary-action')
+    skip_button.click()
+    time.sleep(2)
 
+# load group
+print('Finding group...')
+page = 1
+driver.get('https://www.linkedin.com/groups/'+group'/manage/membership/members/?page='+str(page))
+time.sleep(5)
+dismiss_button = driver.find_element_by_class_name("artdeco-dismiss")
+dismiss_button.click()
+driver.get('https://www.linkedin.com/groups/'+group'/manage/membership/members/?page='+str(page))
+time.sleep(5)
 
-
-
-############# par pages ###############
-
-############# page 1 #############
+# page 1
 if first == 1:
     print('Parsing page 1...')
     x = 67
@@ -86,7 +69,7 @@ if first == 1:
             csv_writer.writerow([parsename(x), parsebio(x)])
     first = 2
 
-############# pages 2-155 #############
+# rest of the pages
 a = 0
 for page in range(first, last):
     print('Parsing page '+str(page)+'...')
